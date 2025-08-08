c79564c6677cd9cb2645ef77518b69921e9862ae
ethan <ethan.tang@coconutislandstudio.com>
2025-08-08 17:45:31 +0800

    refactor(FingerRigging): 替换目标变换为距离参数并优化编辑器功能
    - 将目标变换替换为更灵活的距离参数控制
    - 在编辑器中添加距离滑块控件
    - 优化距离计算和序列化逻辑
    - 移除冗余的RecordDistances方法

--- Assets/Game/Scripts/Gameplay/FingerRigging.cs
+++ Assets/Game/Scripts/Gameplay/FingerRigging.cs
@@ -1,3 +1,4 @@
+using UnityEditor;
 using UnityEngine;
 namespace Game.Gameplay
 {
@@ -5,17 +6,45 @@ namespace Game.Gameplay
 	public class FingerRigging : MonoBehaviour
 	{
 #if UNITY_EDITOR
-		[UnityEditor.CustomEditor(typeof(FingerRigging))]
+		[CustomEditor(typeof(FingerRigging))]
 		class Editor : UnityEditor.Editor
 		{
 			public override void OnInspectorGUI()
 			{
 				base.OnInspectorGUI();
+				{
+					var property = serializedObject.FindProperty(nameof(targetDistance));
+					var value = EditorGUILayout.Slider("Target Distance", property.floatValue, 0, ((FingerRigging)target).MaxLength);
+					if (value != property.floatValue)
+					{
+						property.floatValue = value;
+						serializedObject.ApplyModifiedProperties();
+						serializedObject.Update();
+					}
+				}
 				if (GUILayout.Button("Update Finger Rigging"))
 				{
-					var fingerRigging = (FingerRigging)target;
-					fingerRigging.RecordDistances();
-					UnityEditor.EditorUtility.SetDirty(fingerRigging);
+					var target = (FingerRigging)this.target;
+					{
+						var property = serializedObject.FindProperty(nameof(metacarpophalangeal2ProximalInterphalangeal));
+						property.floatValue = Vector3.Distance(
+							target.metacarpophalangealJoint.position,
+							target.proximalInterphalangealJoint.position);
+					}
+					{
+						var property = serializedObject.FindProperty(nameof(proximalInterphalangeal2DistalInterphalangeal));
+						property.floatValue = Vector3.Distance(
+							target.proximalInterphalangealJoint.position,
+							target.distalInterphalangealJoint.position);
+					}
+					{
+						var property = serializedObject.FindProperty(nameof(distalInterphalangeal2Tip));
+						property.floatValue = Vector3.Distance(
+							target.distalInterphalangealJoint.position,
+							target.tip.position);
+					}
+					serializedObject.ApplyModifiedProperties();
+					serializedObject.Update();
 				}
 			}
 		}
@@ -24,10 +53,12 @@ namespace Game.Gameplay
 		[SerializeField] Transform proximalInterphalangealJoint;
 		[SerializeField] Transform distalInterphalangealJoint;
 		[SerializeField] Transform tip;
-		[SerializeField] Transform target;
+		[SerializeField, HideInInspector,] float targetDistance;
 		[SerializeField, HideInInspector,] float metacarpophalangeal2ProximalInterphalangeal;
 		[SerializeField, HideInInspector,] float proximalInterphalangeal2DistalInterphalangeal;
 		[SerializeField, HideInInspector,] float distalInterphalangeal2Tip;
+		public float MaxLength => metacarpophalangeal2ProximalInterphalangeal + proximalInterphalangeal2DistalInterphalangeal + distalInterphalangeal2Tip;
+		void Update() { }
 		void OnDrawGizmos()
 		{
 			var a = metacarpophalangealJoint;
@@ -41,27 +72,19 @@ namespace Game.Gameplay
 			if (b) Gizmos.DrawSphere(b.position, 0.001f);
 			if (c) Gizmos.DrawSphere(c.position, 0.001f);
 			if (d) Gizmos.DrawSphere(d.position, 0.001f);
-			if (target)
+			if (a && d)
 			{
+				var ray = new Ray(a.position, d.position - a.position);
+				var preferredPosition = ray.GetPoint(targetDistance);
 				Gizmos.color = Color.blue;
-				Gizmos.DrawSphere(target.position, 0.001f);
-				if (d) Gizmos.DrawLine(target.position, d.position);
+				Gizmos.DrawSphere(preferredPosition, 0.001f);
+				Gizmos.DrawLine(a.position, preferredPosition);
 			}
 #if UNITY_EDITOR
-			if (a && b) UnityEditor.Handles.Label((a.position + b.position) * 0.5f, metacarpophalangeal2ProximalInterphalangeal.ToString("F2"));
-			if (b && c) UnityEditor.Handles.Label((b.position + c.position) * 0.5f, proximalInterphalangeal2DistalInterphalangeal.ToString("F3"));
-			if (c && d) UnityEditor.Handles.Label((c.position + d.position) * 0.5f, distalInterphalangeal2Tip.ToString("F3"));
+			if (a && b) Handles.Label((a.position + b.position) * 0.5f, metacarpophalangeal2ProximalInterphalangeal.ToString("F2"));
+			if (b && c) Handles.Label((b.position + c.position) * 0.5f, proximalInterphalangeal2DistalInterphalangeal.ToString("F3"));
+			if (c && d) Handles.Label((c.position + d.position) * 0.5f, distalInterphalangeal2Tip.ToString("F3"));
 #endif
-		}
-		void RecordDistances()
-		{
-			metacarpophalangeal2ProximalInterphalangeal = Vector3.Distance(metacarpophalangealJoint.position, proximalInterphalangealJoint.position);
-			proximalInterphalangeal2DistalInterphalangeal = Vector3.Distance(proximalInterphalangealJoint.position, distalInterphalangealJoint.position);
-			distalInterphalangeal2Tip = Vector3.Distance(distalInterphalangealJoint.position, tip.position);
-		}
-		void Update()
-		{
-			
 		}
 	}
 }
