3af0d343dab4b7d8fd8655654034c6023a5ef2d6
ethan <ethan.tang@coconutislandstudio.com>
2025-08-08 16:24:17 +0800

    feat(PlayerIKController): 实现基于腿长的角色根节点位置计算
    添加腿长参数并计算角色根节点位置，使其基于双脚位置和腿长动态调整高度。同时添加调试绘制功能帮助可视化计算。

--- Assets/Game/Scripts/Gameplay/PlayerIKController.cs
+++ Assets/Game/Scripts/Gameplay/PlayerIKController.cs
@@ -1,3 +1,5 @@
+using System;
+using Game.Utilities;
 using UnityEngine;
 namespace Game.Gameplay
 {
@@ -6,9 +8,22 @@ namespace Game.Gameplay
 		[SerializeField] Transform leftFoot;
 		[SerializeField] Transform rightFoot;
 		[SerializeField] Transform playerRoot;
+		[SerializeField] float legLength;
 		void Update()
 		{
-			//playerRoot.position = (leftFoot.position + rightFoot.position)
+			var mid = (leftFoot.position + rightFoot.position) * 0.5f;
+			var distance = Vector3.Distance(leftFoot.position, rightFoot.position);
+			var halfDistance = distance * 0.5f;
+			var height = Mathf.Sqrt(Mathf.Max(legLength * legLength - halfDistance * halfDistance, 0));
+			playerRoot.position = mid.WithY(Mathf.Min(leftFoot.position.y, rightFoot.position.y) + (height - legLength));
+		}
+		void OnDrawGizmos()
+		{
+			Gizmos.DrawSphere(playerRoot.position, 0.01f);
+			var distance = Vector3.Distance(leftFoot.position, rightFoot.position);
+			var halfDistance = distance * 0.5f;
+			var height = Mathf.Sqrt(Mathf.Max(legLength * legLength - halfDistance * halfDistance, 0));
+			Gizmos.DrawRay(leftFoot.position, Vector3.up * legLength);
 		}
 	}
 }
