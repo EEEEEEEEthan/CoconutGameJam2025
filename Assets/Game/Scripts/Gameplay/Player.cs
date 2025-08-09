54a5ffd7527d1ebd8f41a1d99aa650e8fa50136e
NextPeter <NextPeter@users.noreply.github.com>
2025-08-09 14:38:34 +0800

    制作动画，动画不工作

--- Assets/Game/Scripts/Gameplay/Player.cs
+++ Assets/Game/Scripts/Gameplay/Player.cs
@@ -6,6 +6,10 @@ namespace Game.Gameplay
 	{
 		[SerializeField] HandIKInput handIKInput;
 		[SerializeField] Animator animator;
+		static readonly int s_walkLeft = Animator.StringToHash("WalkLeft");
+		static readonly int s_walkRight = Animator.StringToHash("WalkRight");
+		static readonly int s_standLeft = Animator.StringToHash("StandLeft");
+		static readonly int s_standRight = Animator.StringToHash("StandRight");
 		void Awake()
 		{
 			handIKInput.LeftGroundDetect.OnTriggerEntered += collider => Debug.Log($"left trigger entered: {collider.name}");
@@ -16,21 +20,53 @@ namespace Game.Gameplay
 		void Update()
 		{
 			if (Input.GetKey(KeyCode.E))
+			{
 				handIKInput.LeftLeg = LegPoseCode.LiftForward;
+				animator.SetBool(s_walkLeft, true);
+				animator.SetBool(s_standLeft, false);
+			}
 			else if (Input.GetKey(KeyCode.W))
+			{
 				handIKInput.LeftLeg = LegPoseCode.LiftUp;
+				animator.SetBool(s_walkLeft, false);
+				animator.SetBool(s_standLeft, true);
+			}
 			else if (Input.GetKey(KeyCode.Q))
+			{
 				handIKInput.LeftLeg = LegPoseCode.LiftBackward;
+				animator.SetBool(s_walkLeft, false);
+				animator.SetBool(s_standLeft, false);
+			}
 			else
+			{
 				handIKInput.LeftLeg = LegPoseCode.Idle;
+				animator.SetBool(s_walkLeft, false);
+				animator.SetBool(s_standLeft, false);
+			}
 			if (Input.GetKey(KeyCode.D))
+			{
 				handIKInput.RightLeg = LegPoseCode.LiftForward;
+				animator.SetBool(s_walkRight, true);
+				animator.SetBool(s_standRight, false);
+			}
 			else if (Input.GetKey(KeyCode.S))
+			{
 				handIKInput.RightLeg = LegPoseCode.LiftUp;
+				animator.SetBool(s_walkRight, false);
+				animator.SetBool(s_standRight, true);
+			}
 			else if (Input.GetKey(KeyCode.A))
+			{
 				handIKInput.RightLeg = LegPoseCode.LiftBackward;
+				animator.SetBool(s_walkRight, false);
+				animator.SetBool(s_standRight, false);
+			}
 			else
+			{
 				handIKInput.RightLeg = LegPoseCode.Idle;
+				animator.SetBool(s_walkRight, false);
+				animator.SetBool(s_standRight, false);
+			}
 			if (Input.GetKeyDown(KeyCode.Space)) handIKInput.Crunch();
 			if (Input.GetKeyUp(KeyCode.Space)) handIKInput.Jump(0.1f);
 		}
