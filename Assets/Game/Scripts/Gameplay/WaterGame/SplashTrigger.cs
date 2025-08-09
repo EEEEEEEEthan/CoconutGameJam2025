b20cd0dce6740d5d1409f335e304ac78bc38dcfa
ethan <tyx193@live.cn>
2025-08-10 04:02:44 +0800

    refactor(WaterGame): 合并碰撞处理逻辑到统一方法
    将OnCollisionEnter和OnCollisionExit中的重复代码提取到HandleSplash方法，通过isEnter参数区分处理方向，减少代码重复

--- Assets/Game/Scripts/Gameplay/WaterGame/SplashTrigger.cs
+++ Assets/Game/Scripts/Gameplay/WaterGame/SplashTrigger.cs
@@ -6,7 +6,7 @@ namespace Game.Gameplay.WaterGame
 {
 	public class SplashTrigger : GameBehaviour
 	{
-		void OnCollisionEnter(Collision other)
+		static void HandleSplash(Collision other, bool isEnter)
 		{
 			var velocity = other.relativeVelocity;
 			if (velocity == default)
@@ -20,7 +20,7 @@ namespace Game.Gameplay.WaterGame
 			{
 				other.GetContacts(contactPoints);
 				var speed = velocity.magnitude;
-				var direction = -velocity.normalized;
+				var direction = isEnter ? -velocity.normalized : velocity.normalized;
 				foreach (var point in contactPoints)
 				{
 					var gameObject = Instantiate(ResourceTable.splashPrefab.Main, point.point - direction * 0.01f, Quaternion.LookRotation(direction));
@@ -30,29 +30,7 @@ namespace Game.Gameplay.WaterGame
 				}
 			}
 		}
-		void OnCollisionExit(Collision other)
-		{
-			var velocity = other.relativeVelocity;
-			if (velocity == default)
-			{
-				if (other.collider.TryGetComponent<VelocityCalculator>(out var calculator))
-					velocity = calculator.Velocity;
-				else
-					return;
-			}
-			using (ListPoolThreaded<ContactPoint>.Rent(out var contactPoints))
-			{
-				other.GetContacts(contactPoints);
-				var speed = velocity.magnitude;
-				var direction = velocity.normalized;
-				foreach (var point in contactPoints)
-				{
-					var gameObject = Instantiate(ResourceTable.splashPrefab.Main, point.point - direction * 0.01f, Quaternion.LookRotation(direction));
-					var particleSystem = gameObject.GetComponent<ParticleSystem>();
-					var emission = particleSystem.emission;
-					emission.rateOverTime = speed.Remapped(0, 0.5f, 100, 1000).Clamped(100, 1000);
-				}
-			}
-		}
+		void OnCollisionEnter(Collision other) => HandleSplash(other, true);
+		void OnCollisionExit(Collision other) => HandleSplash(other, false);
 	}
 }
