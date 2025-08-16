9726c16ae000739c068c7fa549966f93787e558e
ethan <tyx193@live.cn>
2025-08-17 00:37:06 +0800

    refactor(资源管理): 移动手图标资源并更新资源表
    将手图标资源从Textures/hand-icon移动到Resources/HandIcon目录
    更新ResourceTable.cs以反映新的资源路径

--- Assets/Game/Scripts/ResourceManagement/ResourceTable.cs
+++ Assets/Game/Scripts/ResourceManagement/ResourceTable.cs
@@ -4,8 +4,20 @@ namespace Game.ResourceManagement
 	public static class ResourceTable
 	{
 		#region autogen
+		public static readonly Loader<Texture2D> aPng = new("HandIcon/A", "d6251609163cff048b82ca5178c87d36");
+		public static readonly Loader<Texture2D> angryPng = new("HandIcon/angry", "a5e4c11a851624e4fbf1b2f7c2b01a74");
+		public static readonly Loader<Texture2D> hiPng = new("HandIcon/hi", "b4c89dee48e7c4240b041cde62e9ec27");
+		public static readonly Loader<Texture2D> qPng = new("HandIcon/Q", "ffe6258d8e9815e43afb0e5ffe098603");
+		public static readonly Loader<Texture2D> sPng = new("HandIcon/S", "c3acc4c0b96b3a743bddf317115b4faa");
+		public static readonly Loader<Texture2D> shyPng = new("HandIcon/shy", "6fd9d60f5656e3045b30e378ad140487");
+		public static readonly Loader<Texture2D> surprisePng = new("HandIcon/surprise", "cd3e35fa8b4208747be2a47544ea96c0");
+		public static readonly Loader<Texture2D> wPng = new("HandIcon/W", "0805f38d8ffb99445bf4d99e63485d03");
+		public static readonly Loader<Texture2D> xPng = new("HandIcon/X", "c2cb7e4b4b30d6e43b999335e168cb3c");
+		public static readonly Loader<Texture2D> zPng = new("HandIcon/Z", "f346aa4b0552655479f75bc3405396f8");
+		public static readonly Loader<GameObject> notePrefab = new("Note", "e581123282783b847a4c5b3663530c86");
 		public static readonly Loader<GameObject> splashPrefab = new("Splash", "5e32de7ad8c958545bf5188d419e3cf6");
 		public static readonly Loader<GameObject> triggerPrefab = new("Trigger", "6cdf33b72df4eb842a91fd53139ba19e");
+		public static readonly Loader<TextAsset> txt = new("乐谱", "55145f6225e17e546a0f635e531cb609");
 		#endregion autogen
 	}
 }
