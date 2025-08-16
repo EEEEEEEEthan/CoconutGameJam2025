af4cca33c6fb4c49de071fa44ca3f28c631e5a9a
ethan <tyx193@live.cn>
2025-08-16 21:16:00 +0800

    feat: 添加毛球材质并重构毛皮材质系统
    重构毛皮材质系统，删除旧的毛皮材质文件，添加新的毛球材质
    实现FurSurface脚本控制毛发效果，支持多层渲染和参数调整

---
+++ Assets/Game/Scripts/Gameplay/FurSurface.cs
@@ -0,0 +1,54 @@
+using System;
+using UnityEngine;
+namespace Game.Gameplay
+{
+	[ExecuteAlways]
+	public class FurSurface : MonoBehaviour
+	{
+		[SerializeField] MeshRenderer meshRenderer;
+		[SerializeField] Material material;
+		[SerializeField, Range(0, 128),] int layerCount = 10;
+		[SerializeField] Gradient gradient;
+		[SerializeField, Min(0),] float fullLength;
+		[SerializeField] float noiseScale = 1f;
+		[SerializeField] AnimationCurve layerToLength;
+		[SerializeField] AnimationCurve layerToAlphaClip;
+		[SerializeField] float smoothness;
+		[SerializeField] float ambientOcclusion;
+		[SerializeField] float metallic;
+		[SerializeField] float gravityStrength;
+		[SerializeField] float gravityPower;
+		void OnEnable() => Refresh();
+		void OnDisable() => meshRenderer.sharedMaterials = new Material[0];
+		void OnValidate() => Refresh();
+		void Refresh()
+		{
+			if (!enabled) return;
+			if (Application.isPlaying) return;
+			var sharedMaterials = new Material[layerCount];
+			Array.Copy(meshRenderer.sharedMaterials, sharedMaterials, Mathf.Min(meshRenderer.sharedMaterials.Length, layerCount));
+			for (var i = 0; i < layerCount; i++)
+			{
+				Material material;
+				if (sharedMaterials[i] && sharedMaterials[i].shader == this.material.shader)
+					material = sharedMaterials[i];
+				else
+					material = sharedMaterials[i] = new(this.material);
+				var progress = (i + 1f) / layerCount;
+				material.SetFloat("_Length", layerToLength.Evaluate(progress) * fullLength);
+				material.SetFloat("_NoiseScale", noiseScale);
+				material.color = gradient.Evaluate(progress);
+				material.SetFloat("_AlphaClipThreshold", layerToAlphaClip.Evaluate(progress));
+				material.SetFloat("_Smoothness", smoothness);
+				material.SetFloat("_AmbientOcclusion", ambientOcclusion);
+				material.SetFloat("_Metallic", metallic);
+				material.SetFloat("_GravityStrength", gravityStrength);
+				material.SetFloat("_GravityPower", gravityPower);
+			}
+			meshRenderer.sharedMaterials = sharedMaterials;
+			var bounds = meshRenderer.bounds;
+			bounds = new(bounds.center, bounds.size + Vector3.one * fullLength * 2);
+			meshRenderer.bounds = bounds;
+		}
+	}
+}
