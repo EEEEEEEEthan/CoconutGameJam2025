546d28569d96cea8b3c5748e8238a85e4ec5d81b
ethan <tyx193@live.cn>
2025-08-17 19:04:04 +0800

    feat(材质): 重构毛皮材质系统并添加UV类型选项
    - 将中文命名的"毛皮.mat"迁移为英文命名的"FurMaterial.mat"资源
    - 在ResourceTable中添加furMaterialMat资源引用
    - 扩展FurSurface脚本支持多种UV映射类型(UV/ObjectXY/ObjectXZ/WorldXY/WorldXZ)
    - 更新FurShader.shadergraph添加UV类型切换功能
    - 优化材质参数默认值和编辑器刷新逻辑

--- Assets/Game/Scripts/Gameplay/FurSurface.cs
+++ Assets/Game/Scripts/Gameplay/FurSurface.cs
@@ -1,27 +1,63 @@
 using System;
+using System.Linq;
+using Game.ResourceManagement;
 using UnityEngine;
 namespace Game.Gameplay
 {
 	[ExecuteAlways]
 	public class FurSurface : MonoBehaviour
 	{
-		[SerializeField] Material material;
+		public enum UVType
+		{
+			Uv,
+			ObjectXy,
+			ObjectXz,
+			WorldXy,
+			WorldXz,
+		}
+		[SerializeField] UVType uvtype;
 		[SerializeField, Range(0, 128),] int layerCount = 10;
-		[SerializeField] Gradient gradient;
-		[SerializeField, Min(0),] float fullLength;
+		[SerializeField] Gradient gradient = new()
+		{
+			alphaKeys = new GradientAlphaKey[]
+			{
+				new(1, 1),
+				new(0, 1),
+			},
+			colorKeys = new GradientColorKey[]
+			{
+				new(Color.red, 0),
+				new(Color.green, 1),
+			},
+		};
+		[SerializeField, Min(0),] float fullLength = 0.1f;
 		[SerializeField] Vector4 noiseScales = new(100, 100, 100, 100);
 		[SerializeField] Vector4 noiseWeights = new(1, 1, 1, 1);
-		[SerializeField] AnimationCurve layerToLength;
-		[SerializeField] AnimationCurve layerToAlphaClip;
-		[SerializeField] float smoothness;
-		[SerializeField] float ambientOcclusion;
-		[SerializeField] float metallic;
+		[SerializeField] AnimationCurve layerToLength = new()
+		{
+			keys = new Keyframe[]
+			{
+				new(0, 0, 0, 999, 0, 0.001f),
+				new(1, 1, 0, 0, 0.1f, 0),
+			},
+		};
+		[SerializeField] AnimationCurve layerToAlphaClip = new()
+		{
+			keys = new Keyframe[]
+			{
+				new(0, 0),
+				new(1, 1),
+			},
+		};
+		[SerializeField, Range(0, 1),] float smoothness;
+		[SerializeField, Range(0, 1),] float ambientOcclusion;
+		[SerializeField, Range(0, 1),] float metallic;
 		[SerializeField] float gravityStrength;
-		[SerializeField] float gravityPower;
+		[SerializeField] float gravityPower = 2;
 		MeshRenderer meshRenderer;
 		MeshRenderer MeshRenderer => meshRenderer ??= GetComponent<MeshRenderer>();
 		void OnEnable() => Refresh();
-		void OnDisable() => MeshRenderer.sharedMaterials = new Material[0];
+		void OnDisable() => MeshRenderer.sharedMaterials = Array.Empty<Material>();
 		void OnBecameVisible()
 		{
 			var bounds = MeshRenderer.bounds;
@@ -31,23 +67,30 @@ namespace Game.Gameplay
 			localBounds = new(localBounds.center, localBounds.size + Vector3.one * fullLength);
 			MeshRenderer.localBounds = localBounds;
 		}
+#if UNITY_EDITOR
 		void OnValidate()
 		{
 			if (Application.isPlaying) return;
+			if (!UnityEditor.Selection.gameObjects.Contains(gameObject)) return;
 			Refresh();
 		}
+#endif
 		void Refresh()
 		{
 			if (!enabled) return;
 			var sharedMaterials = new Material[layerCount];
+			var baseMaterial = ResourceTable.furMaterialMat.Main;
 			Array.Copy(MeshRenderer.sharedMaterials, sharedMaterials, Mathf.Min(MeshRenderer.sharedMaterials.Length, layerCount));
 			for (var i = 0; i < layerCount; i++)
 			{
 				Material material;
-				if (sharedMaterials[i] && sharedMaterials[i].shader == this.material.shader)
+				if (sharedMaterials[i] && sharedMaterials[i].shader == baseMaterial.shader)
 					material = sharedMaterials[i];
 				else
-					material = sharedMaterials[i] = new(this.material);
+					material = sharedMaterials[i] = new(baseMaterial)
+					{
+						hideFlags = HideFlags.HideAndDontSave | HideFlags.HideInInspector,
+					};
 				var progress = (i + 1f) / layerCount;
 				material.SetFloat("_Length", layerToLength.Evaluate(progress) * fullLength);
 				material.SetVector("_NoiseScales", noiseScales);
@@ -59,6 +102,7 @@ namespace Game.Gameplay
 				material.SetFloat("_Metallic", metallic);
 				material.SetFloat("_GravityStrength", gravityStrength);
 				material.SetFloat("_GravityPower", gravityPower);
+				material.SetInt("_UVTYPE", (int)uvtype);
 			}
 			MeshRenderer.sharedMaterials = sharedMaterials;
 		}
--- Assets/Game/Scripts/ResourceManagement/ResourceTable.cs
+++ Assets/Game/Scripts/ResourceManagement/ResourceTable.cs
@@ -4,6 +4,7 @@ namespace Game.ResourceManagement
 	public static class ResourceTable
 	{
 		#region autogen
+		public static readonly Loader<Material> furMaterialMat = new("FurMaterial", "dd46e5a62194b2047bd581d7d9335793");
 		public static readonly Loader<Texture2D> aPng = new("HandIcon/A", "d6251609163cff048b82ca5178c87d36");
 		public static readonly Loader<Texture2D> angryPng = new("HandIcon/angry", "a5e4c11a851624e4fbf1b2f7c2b01a74");
 		public static readonly Loader<Texture2D> hiPng = new("HandIcon/hi", "b4c89dee48e7c4240b041cde62e9ec27");
@@ -14,9 +15,7 @@ namespace Game.ResourceManagement
 		public static readonly Loader<Texture2D> wPng = new("HandIcon/W", "0805f38d8ffb99445bf4d99e63485d03");
 		public static readonly Loader<Texture2D> xPng = new("HandIcon/X", "c2cb7e4b4b30d6e43b999335e168cb3c");
 		public static readonly Loader<Texture2D> zPng = new("HandIcon/Z", "f346aa4b0552655479f75bc3405396f8");
-		public static readonly Loader<GameObject> notePrefab = new("Note", "e581123282783b847a4c5b3663530c86");
 		public static readonly Loader<GameObject> splashPrefab = new("Splash", "5e32de7ad8c958545bf5188d419e3cf6");
-		public static readonly Loader<GameObject> triggerPrefab = new("Trigger", "6cdf33b72df4eb842a91fd53139ba19e");
 		public static readonly Loader<TextAsset> txt = new("乐谱", "55145f6225e17e546a0f635e531cb609");
 		#endregion autogen
 	}
